f0cc297dba6eb002924f7fa2287abe22e2e0d8a8
Dakota Martinez <dakotaleemusic@gmail.com>
2018-01-10 00:15:47 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -7,7 +7,8 @@ jewels_in_bag = 100
  
 3.times do 
   puts "Hiding 10 stolen jewels."
-  jewels_in_bag = jewels_in_bag - 10
+  #jewels_in_bag = jewels_in_bag - 10
+  jewels_in_bag -= 10
 end
 
 puts "We have #{jewels_in_bag} jewels still to hide!"
